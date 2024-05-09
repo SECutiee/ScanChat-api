e55a4428c9acaad4d4366e08ba1fd36e59fad395
yingju <yingju.chen@iss.nthu.edu.tw>
2024-05-09 17:57:38 +0800

    writing integration test

---
+++ spec/integration/api_chatrooms_spec.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+require_relative '../spec_helper'
+
+describe 'Test chatrooms Handling' do ###
+  include Rack::Test::Methods
+
+  before do
+    wipe_database
+  end
+
+  describe 'Getting chatrooms' do ###
+    it 'HAPPY: should be able to get list of all chatrooms' do ###
+      ScanChat::Thread.create(DATA[:threads][0]) ### ask
+      ScanChat::Thread.create(DATA[:threads][1]) ### ask
+
+      get 'api/v1/chatrooms' ### ask should it be sth like => api/v1/threads/chatrooms ??
+      _(last_response.status).must_equal 200
+
+      result = JSON.parse last_response.body
+      _(result['data'].count).must_equal 2
+    end
+
+    it 'HAPPY: should be able to get details of a single chatroom' do ###
+      existing_thre = DATA[:threads][1] ###
+      ScanChat::Thread.create(existing_thre) ###
+      id = ScanChat::Thread.first.id ###
+
+      get "/api/v1/chatrooms/#{id}" ### ask same Q in line 17
+      _(last_response.status).must_equal 200
+
+      result = JSON.parse last_response.body
+      _(result['data']['attributes']['id']).must_equal id
+      _(result['data']['attributes']['name']).must_equal existing_thre['name'] ### ask
+    end
+
+    it 'SAD: should return error if unknown chatroom requested' do ###
+      get '/api/v1/chatrooms/foobar' ### ask
+
+      _(last_response.status).must_equal 404
+    end
+
+    it 'SECURITY: should prevent basic SQL injection targeting IDs' do
+      ScanChat::Thread.create(name: 'New Chatroom') ###
+      ScanChat::Thread.create(name: 'Newer Chatroom') ###
+      get 'api/v1/chatrooms/2%20or%20id%3E0' ### ask
+
+      # deliberately not reporting error -- don't give attacker information
+      _(last_response.status).must_equal 404
+      _(last_response.body['data']).must_be_nil
+    end
+  end
+
+  describe 'Creating New Chatrooms' do ###
+    before do
+      @req_header = { 'CONTENT_TYPE' => 'application/json' }
+      @thre_data = DATA[:threads][1] ### ask
+    end
+
+    it 'HAPPY: should be able to create new chatrooms' do ###
+      post 'api/v1/chatrooms', @thre_data.to_json, @req_header ### ask thre_data
+      _(last_response.status).must_equal 201
+      _(last_response.headers['Location'].size).must_be :>, 0
+
+      created = JSON.parse(last_response.body)['data']['data']['attributes']
+      thre = ScanChat::Thread.first ###
+
+      _(created['id']).must_equal thre.id ###
+      _(created['name']).must_equal @thre_data['name'] ### ask
+      _(created['description']).must_equal @thre_data['description'] ### ask
+    end
+
+    it 'SECURITY: should not create chatroom with mass assignment' do ###
+      bad_data = @thre_data.clone ###
+      bad_data['created_at'] = '1900-01-01'
+      post 'api/v1/chatrooms', bad_data.to_json, @req_header ###
+
+      _(last_response.status).must_equal 400
+      _(last_response.headers['Location']).must_be_nil
+    end
+  end
+end
--- spec/integration/api_messages_spec.rb
+++ spec/integration/api_messages_spec.rb
@@ -8,18 +8,21 @@ describe 'Test Message Handling' do
   before do
     wipe_database
 
-    DATA[:threads].each do |thread_data|
-      ScanChat::Thread.create(thread_data) # .save
+    DATA[:threads].each do |thread_data| ###
+      ScanChat::Thread.create(thread_data) ###
     end
   end
 
   it 'HAPPY: should be able to get a list of all messages' do
-    thread = ScanChat::Thread.first
-    DATA[:messages].each do |message_data|
-      thread.add_message(message_data)
+    thre = ScanChat::Thread.first ###
+    DATA[:messages].each do |message_data| ###
+      thre.add_message(message_data) ###
     end
 
-    get "api/v1/threads/#{thread.id}/messages"
+    get "api/v1/chatrooms/#{chatr.id}/messages" ### ask chatr.id
+    _(last_response.status).must_equal 200
+
+    get "api/v1/messageboards/#{mesbor.id}/messages" ### new add
     _(last_response.status).must_equal 200
 
     result = JSON.parse last_response.body
@@ -28,11 +31,14 @@ describe 'Test Message Handling' do
 
   it 'HAPPY: should be able to get details of a single message' do
     mes_data = DATA[:messages][0]
-    thread = ScanChat::Thread.first
+    thre = ScanChat::Thread.first ###
 
-    message = thread.add_message(mes_data)
+    message = thre.add_message(mes_data) ###
 
-    get "api/v1/threads/#{thread.id}/messages/#{message.id}"
+    get "api/v1/chatrooms/#{chatr.id}/messages/#{message.id}"
+    _(last_response.status).must_equal 200
+
+    get "api/v1/messageboards/#{mesbor.id}/messages/#{message.id}" ### new add
     _(last_response.status).must_equal 200
 
     result = JSON.parse last_response.body
@@ -42,26 +48,28 @@ describe 'Test Message Handling' do
   end
 
   it 'SAD: should return error if unknown message requested' do
-    thread = ScanChat::Thread.first
-    get "/api/v1/threads/#{thread.id}/messages/foobar"
+    thre = ScanChat::Thread.first
+    get "/api/v1/chatrooms/#{chatr.id}/messages/foobar" ###
+    get "/api/v1/messageboards/#{mesbor.id}/messages/foobar" ### new add
 
     _(last_response.status).must_equal 404
   end
 
   describe 'Creating New Messages' do
     before do
-      @thread = ScanChat::Thread.first
+      @thre = ScanChat::Thread.first ###
       @mes_data = DATA[:messages][1]
       @req_header = { 'CONTENT_TYPE' => 'application/json' }
     end
 
-    it 'HAPPY: should b e able to create a new message' do
-      post "/api/v1/threads/#{@thread.id}/messages", @mes_data.to_json, @req_header
+    it 'HAPPY: should be able to create a new message' do
+      post "/api/v1/chatrooms/#{@chatr.id}/messages", @mes_data.to_json, @req_header
+      post "/api/v1/messageboards/#{@mesbor.id}/messages", @mes_data.to_json, @req_header ### new add
       _(last_response.status).must_equal 201
       _(last_response.headers['Location'].size).must_be :>, 0
 
       created = JSON.parse(last_response.body)['data']['data']['attributes']
-      mes = ScanChat::Message.first
+      mes = ScanChat::Message.first ###
 
       _(created['id']).must_equal mes.id
       _(created['content']).must_equal @mes_data['content']
@@ -71,7 +79,10 @@ describe 'Test Message Handling' do
     it 'SECURITY: should not create messages with mass assignment' do
       bad_data = @mes_data.clone
       bad_data['created_at'] = '1900-01-01'
-      post "api/v1/threads/#{@thread.id}/messages",
+      post "api/v1/chatrooms/#{@chatr.id}/messages",
+           bad_data.to_json, @req_header
+
+      post "api/v1/messageboards/#{@mesbor.id}/messages", ###
            bad_data.to_json, @req_header
 
       _(last_response.status).must_equal 400
