2e2ce7e5b1bce1c4e3803a90d01e5559f78daa50
Tristan <tristan.doering@tum.de>
2024-05-23 15:14:31 +0800

    try to fix db

--- app/db/migrations/006_accounts_chatrooms_join_create.rb
+++ app/db/migrations/006_accounts_chatrooms_join_create.rb
@@ -5,8 +5,6 @@ require 'sequel'
 Sequel.migration do
   change do
     create_join_table(member_id: :accounts, chatroom_id: :chatrooms) do
-      column :member_id, 'uuid', null: false
-      column :chatroom_id, 'uuid', null: false
     end
   end
 end
