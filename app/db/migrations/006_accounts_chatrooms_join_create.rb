a901c1f9bfe53b4eed3b3817e19a1d9d9383c268
Tristan <tristan.doering@tum.de>
2024-05-23 15:09:28 +0800

    fix uuid db problem

--- app/db/migrations/006_accounts_chatrooms_join_create.rb
+++ app/db/migrations/006_accounts_chatrooms_join_create.rb
@@ -6,6 +6,7 @@ Sequel.migration do
   change do
     create_join_table(member_id: :accounts, chatroom_id: :chatrooms) do
       column :member_id, 'uuid', null: false
+      column :chatroom_id, 'uuid', null: false
     end
   end
 end
