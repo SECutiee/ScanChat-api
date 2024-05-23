67ac67a179982ae5a1f5750fbd66633cb031eb62
Tristan <tristan.doering@tum.de>
2024-05-23 16:27:13 +0800

    db fix??

--- app/db/migrations/006_accounts_chatrooms_join_create.rb
+++ app/db/migrations/006_accounts_chatrooms_join_create.rb
@@ -4,16 +4,14 @@ require 'sequel'
 
 Sequel.migration do
   change do
-    create_join_table(member_id: :accounts, chatroom_id: :chatrooms) do
+    create_join_table(member_id: { table: :accounts, type: :uuid }, chatroom_id: { table: :chatrooms, type: :uuid }) do
       # Define columns explicitly with UUID type
-      column :member_id, 'uuid', null: false
-      column :chatroom_id, 'uuid', null: false
+      # column :member_id, 'uuid', null: false
+      # column :chatroom_id, 'uuid', null: false
 
-      # Add foreign key constraints
-      add_foreign_key [:member_id], :accounts, type: 'uuid'
-      add_foreign_key [:chatroom_id], :chatrooms, type: 'uuid'
+      # # Add foreign key constraints
+      # add_foreign_key [:member_id], :accounts, type: 'uuid'
+      # add_foreign_key [:chatroom_id], :chatrooms, type: 'uuid'
     end
-    set_column_type :accounts_chatrooms, :member_id, :uuid
-    set_column_type :accounts_chatrooms, :chatroom_id, :uuid
   end
 end
